f5c3ac5421212ce1d11b7e4e59225ab9d5f14cbb
kedmar <m.kedzierski76@gmail.com>
2023-01-11 11:37:52 +0100

    Update styles, js and html

--- popup101.js
+++ popup101.js
@@ -1,1 +1,3 @@
-console.log('popup101');
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
