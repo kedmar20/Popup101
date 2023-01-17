fc484e926b5ade57ac0c35199dfa056e2cd404d1
kedmar <m.kedzierski76@gmail.com>
2023-01-17 10:54:46 +0100

    Update index.html, style.css, script.js

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
-const belsp2 = require('./buttons.js')
+/* modal, sposób nr 1, wg 'Mata' ale po mojemu*/
 
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-
-console.log(belsp2());
-
-const http = require('http')
-const port = 3000
+const addToBasket = document.querySelector('.action-btn');
+// const secondAction = document.querySelector('')
+// const default =
 
 
-const handler = (request, response) => {
-    console.log('new user!');
-    response.end('Hello node')
+const funktionAddContainer = (romek) => {    
+    let newElement = document.createElement('div');
+    newElement.classList.add('modal-container', 'remove-modul')
+    
+    newElement.innerHTML = `
+        <div class="modal">
+            <p class="modal-title">
+                <i class="model-title-icon" data-feather=${romek.iconName}></i>
+                ${romek.title || 'to nie ten tytul'}</p>
+            <p class="modal-description">${romek.description  || 'BRAK DESCRIPTION'}                
+            </p>
+            <div class="modal-buttons">
+                <button class="${romek.Button1class} remove-modul">${romek.button1}</button>
+                <button class="${romek.Button2class} remove-modul">${romek.button2}</button>
+            </div>
+            <button class="modal-close-icon remove-modul"><i data-feather="x"></i></button>
+        </div>`;
+    document.body.appendChild(newElement)   
+    
+    feather.replace();
+    
+    let removeModul = (tt) => {           
+        // newElement.classList.add('hidden')
+        let dddd = tt.target.classList                      
+        console.log(tt)        
+        if (dddd.contains('remove-modul') || dddd.contains('feather')) {                           
+            newElement.remove(); 
+        }      
+    }    
+
+    let greenButton=document.querySelectorAll('.remove-modul');      
+    greenButton.forEach((btn) =>                 
+            btn.addEventListener('click', removeModul)
+        )  
 }
 
-const server = http.createServer(handler)
-server.listen(port, ()=>{
-    console.log('server is running...');
-})
+let funktionAddToBasket = () => {
+    funktionAddContainer({
+        iconName: 'info' ,
+        titlee: 'Add To Basket'  ,   
+        descriptioNn: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem libero sit eligendi labore, expedita iure ab nam obcaecati numquam, qui reprehenderit perspiciatis ipsa ea! Facere?',   
+        button1: 'ACCEPT',
+        button2: 'CANCEL',
+        Button1class: 'modal-accept',
+        Button2class: 'modal-decline',
+    })}
+
+addToBasket.addEventListener('click', funktionAddToBasket);
+// secondAction.addEventListener('click', funktionsecondAction);
+// default.addEventListener('click', funktionDefault);
+
+
+
+
