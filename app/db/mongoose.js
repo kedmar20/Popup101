090d1e713d00064789f727d5714609775b461671
kedmar20 <m.kedzierski76@gmail.com>
2023-02-26 08:10:34 +0100

    mongoose gitbash

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { database } = require("../config");
 
 mongoose.set("strictQuery", false);
-
+//
 mongoose.connect(database, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
